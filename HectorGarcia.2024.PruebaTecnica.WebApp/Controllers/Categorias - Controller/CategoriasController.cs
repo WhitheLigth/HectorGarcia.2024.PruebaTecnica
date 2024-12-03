7d6bb3c9f722f761eb4f96cf3c54a001f98b0923
Hector Garcia <hg425823@gmail.com>
2024-12-03 00:57:29 -0600

    INSERCION DE LOGICA PARA LOS CONTROLADORES PARA CATEGORIAS Y PRODUCTOS

---
+++ HectorGarcia.2024.PruebaTecnica.WebApp/Controllers/Categorias - Controller/CategoriasController.cs
@@ -0,0 +1,111 @@
+﻿#region REFERENCIAS
+// Referencias Necesarias Para El Correcto Funcionamiento
+using HectorGarcia._2024.PruebaTecnica.BL.Categorias___BL;
+using HectorGarcia._2024.PruebaTecnica.EN.Categorias___EN;
+using Microsoft.AspNetCore.Mvc;
+
+#endregion
+
+namespace HectorGarcia._2024.PruebaTecnica.WebApp.Controllers.Categorias___Controller
+{
+    public class CategoriasController : Controller
+    {
+        // Instancia de la clase logica de negocio
+        CategoriasBL categoriasBL = new CategoriasBL();
+
+        #region METODO PARA INDEX
+        // Metodo Para Mostrar La Vista Index
+        public async Task<IActionResult> Index(Categorias categorias = null!)
+        {
+            if (categorias == null)
+                categorias = new Categorias();
+
+            var categoriass = await categoriasBL.BuscarAsync(categorias);
+            return View(categoriass);
+        }
+        #endregion
+
+        #region METODO PARA GUARDAR
+        // Metodo Para Mostrar La Vista Guardar
+        public IActionResult Crear()
+        {
+            ViewBag.Error = "";
+            return View();
+        }
+
+        // Metodo Que Recibe y Envia a La Base De Datos
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear(Categorias categorias)
+        {
+            try
+            {
+                int result = await categoriasBL.CrearAsync(categorias);
+                TempData["SuccessMessageCreate"] = "Categoria Agregada Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(categorias);
+            }
+        }
+        #endregion
+
+        #region METODO PARA MODIFICAR
+        // Metodo Para Mostrar La Vista De Modificar
+        public async Task<IActionResult> Edit(int id)
+        {
+            var categorias = await categoriasBL.ObtenerPorIdAsync(new Categorias { Id = id });
+            ViewBag.Error = "";
+            return View(categorias);
+        }
+
+        // Metodo Que Recibe y Envia a La Base De Datos
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Categorias categorias)
+        {
+            try
+            {
+                int result = await categoriasBL.ModificarAsync(categorias);
+                TempData["SuccessMessageUpdate"] = "Categoria Modificada Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(categorias);
+            }
+        }
+        #endregion
+
+        #region METODO PARA ELIMINAR
+        // Metodo Para Mostrar La Vista De Eliminar
+        public async Task<IActionResult> Delete(int id)
+        {
+            var categorias = await categoriasBL.ObtenerPorIdAsync(new Categorias { Id = id });
+            ViewBag.Error = "";
+            return View(categorias);
+        }
+
+        // Metodo Que Recibe y Envia a La Base De Datos
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, Categorias categorias)
+        {
+            try
+            {
+                int result = await categoriasBL.EliminarAsync(categorias);
+                TempData["SuccessMessageDelete"] = "Categoria Eliminada Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(categorias);
+            }
+        }
+        #endregion
+    }
+}
---
+++ HectorGarcia.2024.PruebaTecnica.WebApp/Controllers/Productos - Controller/ProductosController.cs
@@ -0,0 +1,126 @@
+﻿#region REFERENCIAS
+// Referencias Necesarias Para El Correcto Funcionamiento
+using HectorGarcia._2024.PruebaTecnica.BL.Categorias___BL;
+using HectorGarcia._2024.PruebaTecnica.BL.Productos___BL;
+using HectorGarcia._2024.PruebaTecnica.EN.Categorias___EN;
+using HectorGarcia._2024.PruebaTecnica.EN.Productos___EN;
+using Microsoft.AspNetCore.Mvc;
+
+#endregion
+
+namespace HectorGarcia._2024.PruebaTecnica.WebApp.Controllers.Productos___Controller
+{
+    public class ProductosController : Controller
+    {
+        // Instancia de la clase logica de negocio
+        ProductosBL productosBL = new ProductosBL();
+        CategoriasBL categoriasBL = new CategoriasBL();
+
+        #region METODO PARA MOSTRAR INDEX
+        // Accion Para Mostrar La Vista Index
+        public async Task<IActionResult> Index(Productos productos = null!)
+        {
+            if (productos == null)
+                productos = new Productos();
+
+            var productoss = await productosBL.BuscarIncludeAsync(productos);
+            var categorias = await categoriasBL.ListarAsync();
+
+            ViewBag.Categorias = categorias;
+            return View(productoss);
+        }
+        #endregion
+
+        #region METODO PARA CREAR
+        // Accion Para Mostrar La Vista De Crear
+        public async Task<IActionResult> Crear()
+        {
+            var categorias = await categoriasBL.ListarAsync();
+            ViewBag.Categorias = categorias;
+            ViewBag.Error = "";
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear(Productos productos)
+        {
+            try
+            {
+                int result = await productosBL.CrearAsync(productos);
+                TempData["SuccessMessageCreate"] = "Producto Agregado Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = e.Message;
+                ViewBag.Categorias = await categoriasBL.ListarAsync();
+                return View(productos);
+            }
+        }
+        #endregion
+
+        #region METODO PARA MODIFICAR
+        // Accion Que Muestra El Formulario
+        public async Task<IActionResult> Modificar(int id)
+        {
+            var productos = await productosBL.ObtenerPorIdAsync(new Productos { Id = id });
+            productos.Categorias = await categoriasBL.ObtenerPorIdAsync(new Categorias { Id = productos.Id });
+            ViewBag.Categorias = await categoriasBL.ListarAsync();
+            return View(productos);
+        }
+
+        // Accion Que Recibe Los Datos y Los Envia a La Base De Datos
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Modificar(int id, Productos productos)
+        {
+            try
+            {
+                int result = await productosBL.ModificarAsync(productos);
+                TempData["SuccessMessageUpdate"] = "Producto Modificado Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = e.Message;
+                ViewBag.Categorias = await categoriasBL.ListarAsync();
+                return View(productos);
+            }
+        }
+        #endregion
+
+        #region METODO PARA ELIMINAR
+        // Accion Que Muestra El Formulario
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            var productos = await productosBL.ObtenerPorIdAsync(new Productos { Id = id });
+            productos.Categorias = await categoriasBL.ObtenerPorIdAsync(new Categorias { Id = productos.Id });
+            return View(productos);
+        }
+
+        // Accion Que Recibe Los Datos y Los Envia a La Base De Datos
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Eliminar(int id, Productos productos)
+        {
+            try
+            {
+                int result = await productosBL.EliminarAsync(productos);
+                TempData["SuccessMessageDelete"] = "Producto Eliminado Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = e.Message;
+                var productosDb = await productosBL.ObtenerPorIdAsync(productos);
+                if (productosDb == null)
+                    productosDb = new Productos();
+                if (productosDb.Id > 0)
+                    productosDb.Categorias = await categoriasBL.ObtenerPorIdAsync(new Categorias { Id = productosDb.IdCategoria });
+                return View(productosDb);
+            }
+        }
+        #endregion
+    }
+}
